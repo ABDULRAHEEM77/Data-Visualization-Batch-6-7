16a6ccf907a876ca76d49742c7120321ef97b928
Abdul Raheem Dadani <abdurraheemdadani26@gmail.com>
2026-02-28 23:01:10 +0500

    Fix Bug

--- Donut_chart.py
+++ Donut_chart.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 sizes = [30, 20, 25, 25]
 labels = ['Python', 'Java', 'C++', 'Javascript']
 
-fig, ax = plt.subplot()
+fig, ax = plt.subplots()
 wedges, texts = ax.pie(sizes, labels=labels)
 
 centre_circle = plt.Circle((0,0), 0.60, fc='white')
